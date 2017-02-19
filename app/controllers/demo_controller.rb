c6f3a0df92b171b6d40d8e3bc32a4290b375f329
Andrew Batallones <batallonesa@live.com>
2017-02-18 19:30:44 -0800

    CVM stuff

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,7 +1,24 @@
 class DemoController < ApplicationController
 
   layout false
-  
+
   def index
   end
+
+  def hello
+    @array = [1, 2, 3, 4, 5] # this is an instance variable
+    @id = params['id']
+    @page = params[:page]
+  end
+
+  def other_hello
+    redirect_to(:action => 'index')
+    # the demo controller is called by default because we are in demo_controller.rb
+  end
+
+  # Can also call to be directed to external links
+  def lynda
+    redirect_to('https://www.lynda.com')
+  end
+
 end
