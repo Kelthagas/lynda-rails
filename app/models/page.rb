8d9229cdfe76218a5f8c9dbed7dbe14355066f6d
Andrew Batallones <batallonesa@live.com>
2017-03-16 20:59:48 -0700

    finished doing database stuff

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,8 @@
 class Page < ApplicationRecord
+
+  # 1:1 and 1:m association
+  # use << to append to parent table
+  belongs_to :subject
+  has_and_belongs_to_many :admin_users
+
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,15 @@
 class Subject < ApplicationRecord
+
+  # 1:1 association
+  # has_one :page
+
+  # 1:M association
+  has_many :pages
+
+  scope :visible, -> { where(visible: true) }
+  scope :invisible, -> { where.not(visible: true) }
+  scope :sorted, -> { order(position: :ASC) }
+  scope :newest_first, -> { order(created_at: :DESC) }
+  scope :search, -> (query) { where( arel_table[:name].matches("%#{query.downcase}%") ) }
+
 end
